dff09deb8b0d05b896ab3f8ea07c86f515449ed1
bbtit <the8butterfly8dream@gmail.com>
2023-01-25 13:12:07 +0900

    edit .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,16 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'standard-with-typescript', 'prettier'],
-  overrides: [],
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react'],
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
